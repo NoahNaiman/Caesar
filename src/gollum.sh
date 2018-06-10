92b7403932f44bdb39c8dc39a6d364a33f881b7e
Noah Naiman <nnaiman@bu.edu>
2018-06-10 13:03:53 -0400

    Tweaking gollum.sh

--- src/gollum.sh
+++ src/gollum.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 #Cleanup file for Sauron on safe exit
 
-sed -n 1,$(awk '/Dynamically appended redirects/{print NR}' server.js)p server.js
+echo 'Initiating cleanup of server.js';
+
+sed -n 1,$(awk '/Dynamically appended redirects/{print NR}'
+server.js)p server.js > ../server.js;
+
+echo 'Initiating cleanup of list.pug';
+
+sed -n 1,8p views/list.pug > views/list.pug;
+
+read -p 'Would you like to delete anything in uploads? [y/n] ' cleanUploads
+
+if [$(echo "$cleanUploads" |tr [:upper:] [:lower:]) = 'y'] || [$(echo "$cleanUploads" | tr [:upper:] [:lower:]) = 'yes'];
+then
+	rm -rf uploads/*
+fi
+
+echo 'Cleanup complete.';
