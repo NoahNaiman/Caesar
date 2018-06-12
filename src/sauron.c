a4939746ba34f483d5036d583d965f8ab8de457e
Noah Naiman <nnaiman@bu.edu>
2018-06-11 22:52:11 -0400

    Added correct routing appends.

--- src/sauron.c
+++ src/sauron.c
@@ -17,16 +17,6 @@ int main(int argc, char const *argv[]){
 	//Server file to append to
 	FILE *server;
 
-	//Buffer to hold new express route to be appended
-	char newRoute[1000] = "\n\napp.get('/";
-
-	//Concatenate new route together
-	char *routePointer = newRoute;
-	routePointer = concatenate(routePointer, (char *)argv[1]);
-	routePointer = concatenate(routePointer, "', (req, res) =>\n\tres.send('localhost:");
-	routePointer = concatenate(routePointer, (char *) argv[3]);
-	routePointer = concatenate(routePointer, "')\n);\n");
-
 	//Open server for appending
 	server = fopen("server.js", "a");
 	printf("Server has been opened for appending\n");
@@ -37,18 +27,28 @@ int main(int argc, char const *argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-	//Append to server
-	printf("Writing to server\n");
-
-	fputs(newRoute, server);
-
-	//Close server
-	fclose(server);
-	printf("Server has been closed for writing\n");
-
-
 	//Execute any necessary commands
 	if(strcmp(argv[2], "none") != 0){
+
+		//Buffer to hold new express route to be appended
+		char newRoute[1000] = "\n\napp.get('/";
+
+		//Concatenate new route together
+		char *routePointer = newRoute;
+		routePointer = concatenate(routePointer, (char *)argv[1]);
+		routePointer = concatenate(routePointer, "', (req, res) =>\n\tres.send('localhost:");
+		routePointer = concatenate(routePointer, (char *) argv[3]);
+		routePointer = concatenate(routePointer, "')\n);\n");
+
+		//Append to server
+		printf("Writing to server\n");
+
+		fputs(newRoute, server);
+
+		//Close server
+		fclose(server);
+		printf("Server has been closed for writing\n");
+
 
 		printf("Launching %s\n", argv[1]);
 
@@ -63,6 +63,27 @@ int main(int argc, char const *argv[]){
 		printf("%s\n", toExecute);
 
 		system(toExecute);
+	}
+	else{
+		//Buffer to hold new express route to be appended
+		char newRoute[1000] = "\n\napp.get('/";
+
+		//Concatenate new route together
+		char *routePointer = newRoute;
+		routePointer = concatenate(routePointer, (char *)argv[1]);
+		routePointer = concatenate(routePointer, "', (req, res) =>\n\tres.sendFile(__dirname + 'uploads/");
+		routePointer = concatenate(routePointer, (char *) argv[1]);
+		routePointer = concatenate(routePointer, "/index.html')\n);\n");
+
+		//Append to server
+		printf("Writing to server\n");
+
+		fputs(newRoute, server);
+
+		//Close server
+		fclose(server);
+		printf("Server has been closed for writing\n");
+
 	}
 
 	return 0;
